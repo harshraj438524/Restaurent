1894e7383e9368d67e06f8500242e764147979d6
harshraj438524 <21bec050@iiitdmj.ac.in>
2023-01-27 19:43:34 +0530

    hover fuction added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant"; 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./component/Hooks/useReducer";
+import Navbar from "./component/Basics/Navbar";
+
+const App = () => {
+  return(
+    
+    // <UseReducer />
+   <Resturant/>
+
+  );
+};
+
+export default App;
